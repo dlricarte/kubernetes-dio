637c65212c1fac5f206f50ccec0025edbf0aefc5
Demetrius Limoeiro Ricarte <demetrius.ricarte@zpeceara.com.br>
2025-01-06 10:44:28 -0300

    Projeto Kubernates DIO

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Criando as imagens........"
+
+docker build -t dlricarte/projeto-backend:1.0 backend/.
+docker build -t dlricarte/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens......"
+
+docker push dlricarte/projeto-backend:1.0
+docker push dlricarte/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes...."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments..........."
+
+kubectl apply -f ./deployments.yml
+
+
